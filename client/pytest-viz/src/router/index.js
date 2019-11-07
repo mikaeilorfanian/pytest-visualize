1982f71ed8a86c32bea513854e324846c2f2fa16
Mikaeil Orfanian <mokt@outlook.com>
2019-11-07 20:56:34 +0100

    Add front-end project skeleton

---
+++ client/pytest-viz/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Run from '@/components/Run'
+
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+      {
+        path: '/',
+        name: 'root',
+        component: Run
+      },
+    ]
+})
