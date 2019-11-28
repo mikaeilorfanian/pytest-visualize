201bec7e923864eeefb2b86c156549d358dbf000
Mikaeil Orfanian <mokt@outlook.com>
2019-11-28 22:05:00 +0100

    PRR-make the COR allowed IP address configurable via env vars

--- server/api.py
+++ server/api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import os
+
+from flask import Flask
 from flask import g
 from flask import request
 from flask_cors import CORS
@@ -6,9 +8,12 @@ from flask_socketio import SocketIO
 import pytest
 
 
+CORS_IP = os.environ.get('CORS_IP') or 'http://localhost:8080'
+
+
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins=['http://localhost:8080'])  # the IP of local front-end server
+socketio = SocketIO(app, cors_allowed_origins=[CORS_IP])  # the IP of local front-end server
 
 
 @app.route('/tests')
