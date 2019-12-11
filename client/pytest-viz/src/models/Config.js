25675e44f969a88dbec28c11d41acb6703b3601f
Mikaeil Orfanian <mokt@outlook.com>
2019-12-11 09:58:37 +0100

    cleanup-code formatting

--- client/pytest-viz/src/models/Config.js
+++ client/pytest-viz/src/models/Config.js
@@ -1,16 +1,16 @@
 export default {
-    getAuto(){
-        const auto = localStorage.getItem('auto');
-        if (auto){
-            if (auto === 'false'){
-                return null;
-            }
-            else{
-                return 'true';
-            }
-        }
-        else{
-            return null;
-        }
-    },
-  }
+  getAuto(){
+    const auto = localStorage.getItem('auto');
+    if (auto){
+      if (auto === 'false'){
+        return null;
+      }
+      else{
+        return 'true';
+      }
+    }
+    else{
+      return null;
+    }
+  },
+}
